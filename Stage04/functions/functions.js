fc63e2868a664deece686b00d170be190a83622e
Luis junior <luisroyale211@gmail.com>
2023-07-20 20:00:46 -0300

    conteúdo do dia 20/07

---
+++ Stage04/functions/functions.js
@@ -0,0 +1,10 @@
+//da apra criar uma função a partir de uma var, é uma func anônima, já que n tem nome.
+
+//parametros da função são o 'number1' e 'number2'
+const sum = function( number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2, 3) // aqui nessa função sum são os argumentos
+
+//parei no 117
---
+++ Stage04/functions/functions_2.js
@@ -0,0 +1,23 @@
+
+//Por que as funções existem?
+
+//serve para repetir o código de uma melhor forma, que não seja um ctrl+c / ctrl+v
+
+
+//declaração de uma função
+function  createPhrases() {
+    console.log('hahaha')
+    console.log('hahahahahaha')
+    console.log('hahahahahahahaha')
+}
+
+//executar a função.
+//da para chama-lá diversas vezes só pelo nome que foi dado a mesma.
+createPhrases()
+// createPhrases()
+// createPhrases()
+// createPhrases()
+
+console.log('Fim do programa')
+
+
