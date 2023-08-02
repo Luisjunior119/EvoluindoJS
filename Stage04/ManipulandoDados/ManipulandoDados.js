596a77ef33e8e8f1d93c659d3ef9c7614aaa7fdf
Luis junior <luisroyale211@gmail.com>
2023-08-01 22:26:12 -0300

    Content uptade

---
+++ Stage04/ManipulandoDados/ManipulandoDados.js
@@ -0,0 +1,69 @@
+//AULA 123 PROTOTYPE
+
+/* 
+    Prototype
+
+    * prototytpe-based lenguage
+    * prototype chain
+    * ___proto__
+*/ 
+
+//AULA 124 TYPE CONVERSION COERSION
+
+/* 
+    Type Conversion (typecasting) vs type coersion
+
+    *alteração de um tipo de dado para outro
+
+*/
+     console.log('9' + 5)
+
+//AULA 125 strings em números.
+
+//MANIPULANDO STRINGS E NÚMEROS
+
+// TRANSFORMAR STRING EM NÚMERO E NÚMERO EM STRING
+
+// let string = '123'
+// console.log(Number(string))
+// let number = 321
+// console.log(String(number))
+
+//AULA 126 CONTAR QUANTOS CARACTERES TEM UMA PALAVRA E QUAINTOS DÍGITOS TEM UM NÚMERO
+
+// let word = 'paralelepipedo'
+// console.log(word.length)
+// let number = 1234
+// console.log(String(number).length)
+
+//AULA 127 CASAS DECIMAIS
+
+// TRANSFORMAR UM NÚMERO QUEBRADO COM 2 CASAS DECIMAIS E TROCAR PONTO POR VÍRGULA.
+
+// let number = 28392.0209390293
+// console.log(number.toFixed(3).replace(".", ","))
+
+//AULA 128 TRANSFORME LETRAS MINÚSCULAS EM MAIÚSCULAS. FAÇA O CONTRÁRIO DISSO TAMBÉM.
+
+// let word = 'programar é top'
+
+// console.log(word.toUpperCase())
+
+//AULA 129 
+
+// SEPARE UM TEXTO QUE CONTÉM ESPAÇOS, EM UM NOVO ARRAY ONDE CADA TEXTO É UMA POSIÇÃO DO ARRAY.
+//DEPOIS DISSO, TRANSFORMA O ARRAY EM UM TEXTO E ONDE ERAM ESPAÇOS, COLOQUE _
+
+// let textinho = "eu quero um emprego!"
+// let myArray = textinho.split(" ")
+// let textinhoWithUnderscore = myArray.join("_")
+// console.log(textinhoWithUnderscore.toUpperCase())
+
+//AULA 130 VERIFICAR SE O TEXTO CONTÉM A PALAVRA AMOR
+//includes verifica se está maiúsculo ou minúsculo
+
+// let phrase = "Eu quero viver o Amor!"
+// console.log(phrase.includes("Amor"))
+
+//PAREI NA AULA 131
+
--- Stage04/functions/functions.js
+++ Stage04/functions/functions.js
@@ -2,9 +2,94 @@
 
 //parametros da função são o 'number1' e 'number2'
 const sum = function( number1, number2) {
-    console.log(number1 + number2)
+    total = number1 + number2
+    return total
 }
 
-sum(2, 3) // aqui nessa função sum são os argumentos
 
-//parei no 117
+let number1 = 34
+let number2 = 25
+sum(number1, number2) // aqui nessa função sum são os argumentos
+
+console.log(`o número 1 é ${number1}`)
+console.log(`o número 2 é ${number2}`)
+console.log(`a soma é: ${sum(number1, number2)}`)
+
+//aula 117.
+
+// Função é um liquidificador
+// Fruta 1 e 2 são os parâmetros, na const 'copo' são passados os argumentos.
+
+
+function fazerSuco (fruta1, fruta2) {
+    return fruta1 + fruta2
+}
+
+const copo = fazerSuco ("banana", "maçã")
+
+console.log(copo)
+
+//AULA 118 FUNCTION SCOPE
+
+let subject = 'create video'
+
+function createThink(subject) {
+    subject = 'study'
+    return subject
+}
+
+console.log(subject)
+console.log(createThink(subject))
+
+//AULA 119 FUNCTION HOISTING
+
+// SayMyName();
+
+// function SayMyName() {
+//     console.log('Luis')
+// }
+
+//AULA 120 ARROW FUNCTION
+
+const SayYourName = () => {
+    console.log('Luis')
+} 
+
+SayYourName();
+
+//AULA 121 callback function
+//callback é uma função que passa valor a outra. Ou seja, chamar uma função e depois outra de volta
+
+// function SayMyName(name) {
+//     console.log('antes de executar a função callback')
+//     name()
+
+//     console.log('depois de executar o callback')
+// }
+// SayMyName(
+//     () => {
+//         console.log('Estou em uma callback')
+//     }
+//)
+
+//AULA 122 FUNÇÕES CONSTRUTORAS
+
+/* 
+    Function() constructor
+
+    * expressão new
+    * criar um novo objeto
+    * this keyword
+
+*/
+
+function Person(name) {
+    this.name = name
+    this.walk = function() {
+        return this.name + "está andando"
+    }
+}
+const Luis = new Person("Luis")
+const Jane = new Person('Jane')
+console.log(Jane.walk)
+console.log(Luis.walk)
